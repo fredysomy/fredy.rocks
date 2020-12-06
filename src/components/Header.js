b7a130c83b75265f06c2e98b26acf3aebac0631b
fredysomy <fredysomy@gmail.com>
2020-12-06 18:20:46 +0000

    added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,33 @@
 import React,{Component} from 'react'
+import './Header.css'
+export default class Header extends Component{
+  render(){
+    return(
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a id="Head" class="navbar-brand" href="#">Fredy Somy</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Projects</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      
+    </ul>
+  
+  </div>
+</nav>
+      </div>
+    )
+  }
+
+}
