b92d821c7291437f5ee7813c13f94653adcbc5b7
Fredy Somy <fredysomy@gmail.com>
2020-12-09 19:24:57 +0530

    Update Mainbody.js

--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -8,10 +8,10 @@ export default class Mainbody extends Component{
         <img id="imgme" src="https://avatars2.githubusercontent.com/u/65971072?s=460&u=dd6ae329cb0a04cbddc6c0959a924ceeac62bf02&v=4"/>
         <div  id="det">
           <h1 id ="name">Hi I am <font color="cyan">
- Fredy Somy </font></h1>
+ <u>Fredy Somy</u> </font></h1>
             <hr />
           <h5 >I am  <b>Software Developer</b> and a Student</h5>
-          <h5 id="desc">I foucs mainly on Backend Web development with Python and Javascript . I also work in the Frontend with React . I like to make Desktop apps and Pypi modules.</h5>
+          <h5 id="desc">I focus mainly on Backend Web development with Python and Javascript , also work in the Frontend with React , like to make Desktop apps and Pypi modules.</h5>
           <hr/>
           <div>
             <a href="https://dev.to/fredysomy" ><i id="we" class="fab fa-dev fa-2x"></i></a>
