5c085596cbb1c872fe8dc38777fdff57375f99da
fredysomy <fredysomy@gmail.com>
2020-12-27 18:46:23 +0000

    added projects section

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -6,7 +6,26 @@ export default class Projects extends Component{
         return(
             <div id="main">
                
-                  <h3 id="head" align="center">My <u><font color="cyan">Projects</font></u></h3>
+            <h3 id="head" align="center">My <u><font color="cyan">Projects</font></u></h3>
+                <div className="maindiv">
+                <img className="img-div" src="https://drive.google.com/uc?export=view&id=1CWP-yFwqgy2HKbbHctxHbOW63hwJ4oqX"/>
+                <div className="content-div">
+                        <h3>
+                            MarkdonwIt
+                        </h3>
+                        <p>
+                            Efficient Code Editor to live render Markdown and save as Markdown,Html and Pdf + Instant Hosting in The Web.
+                        </p>
+                        <p>
+                            Usefull for writing docs and Hosting it for free.
+                        </p>
+                        <p id="tech">
+                            Electron JS , HTML,CSS , JavaScript
+                        </p>
+                        <button className="btn btn-primary btn-sm bttn">Source Code</button>
+                        <button className="btn btn-primary btn-sm bttn">Documentation</button>
+                </div>
+            </div>
             
      
        
