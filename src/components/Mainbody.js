1c79e638276baf62b485842d2f61a8db4acf2b78
Fredy Somy <fredysomy@gmail.com>
2020-12-30 08:00:38 +0530

    Update Mainbody.js

--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -5,59 +5,7 @@ export default class Mainbody extends Component{
     
   render(){
     return(
-      <div style={{ position: 'absolute',z-index :'-2'}}>
-        <Particles height="100vh" width="100vw" params={{
-	    "particles": {
-	        "number": {
-	            "value": 160,
-	            "density": {
-	                "enable": false
-	            }
-	        },
-	        "size": {
-	            "value": 3,
-	            "random": true,
-	            "anim": {
-	                "speed": 4,
-	                "size_min": 0.3
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false
-	        },
-	        "move": {
-	            "random": true,
-	            "speed": 1,
-	            "direction": "top",
-	            "out_mode": "out"
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onhover": {
-	                "enable": true,
-	                "mode": "bubble"
-	            },
-	            "onclick": {
-	                "enable": true,
-	                "mode": "repulse"
-	            }
-	        },
-	        "modes": {
-	            "bubble": {
-	                "distance": 250,
-	                "duration": 2,
-	                "size": 0,
-	                "opacity": 0
-	            },
-	            "repulse": {
-	                "distance": 400,
-	                "duration": 4
-	            }
-	        }
-	    }
-}} />
-      </div>
+      
 
 
       <div id="divmain" style={{ position: 'relative', overflow: "hidden" }}>
