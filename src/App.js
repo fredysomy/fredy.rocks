3256106d71f25ec55fd93429cb439b5121152e79
fredysomy <fredysomy@gmail.com>
2020-12-31 18:25:56 +0000

    dda

--- src/App.js
+++ src/App.js
@@ -7,25 +7,65 @@ import Projects from './projects/Projects';
 import Downloads from './donwloads/Downloads';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 function App() {
-    return(
-        <Router>
-        <div className="App">
-        <Header/>
-            <Switch>
-                <Route exact path="/">
-                    
-                <Mainbody />
+    //return(
+     //   <Router>
+       // <div className="App">
+      //  <Header/>
+       //     <Switch>
+        //        <Route exact path="/">
+       //             
+      //          <Mainbody />
+      //     
+      //         
+     //               
+      //              
+       //         </Route>
+//                
+ //               <Route exact path="/downloads" component={Downloads} />
+  //          </Switch>
+   //     </div>
+    //</Router>
+    //)
+
+
+    
+if (window.location.host.split('.')[0] == "projects"){
+        return(
            
-               
-                    
-                    
-                </Route>
-                
-                <Route exact path="/downloads" component={Downloads} />
-            </Switch>
-        </div>
-    </Router>
-    )
+            <Router>
+                 <div className="App">
+                <Header/>
+                <Projects/>
+                </div>
+            </Router>
+           
+        )
+
+    }
+    if (window.location.host.split('.')[0] == "downloads") {
+        return(
+           
+            <Router>
+                 <div className="App">
+                <Header/>
+                <Downloads/>
+                </div>
+            </Router>
+           
+        )
+    }
+    if (window.location.host.split('.')[0] == "fredy") {
+return(
+           
+            <Router>
+                 <div className="App">
+                <Header/>
+                <Mainbody/>
+                </div>
+            </Router>
+           
+        )
+    }
   
 }
 
