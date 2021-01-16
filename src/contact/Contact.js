6bd6110f111830f9c3ef7c191f0786b3b131a06e
Fredy Somy <fredysomy@gmail.com>
2021-01-16 12:40:18 +0530

    Update Contact.js

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -38,16 +38,16 @@ export default class Contact extends Component{
         return(
             <div id="contact">
                 <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" className="form-control" onChange={this.nameonChange.bind(this)}/>
+        
+        <input placeholder="Name" type="text" className="form-control" onChange={this.nameonChange.bind(this)}/>
     </div>
     <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control" aria-describedby="emailHelp" onChange={this.mailonChange.bind(this)} />
+        
+        <input placeholder="Email " type="email" className="form-control" aria-describedby="emailHelp" onChange={this.mailonChange.bind(this)} />
     </div>
     <div className="form-group">
-        <label htmlFor="message">Message</label>
-        <textarea className="form-control" rows="5" onChange={this.messageonChange.bind(this)}></textarea>
+        
+        <textarea placeholder="Your Message" className="form-control" rows="5" onChange={this.messageonChange.bind(this)}></textarea>
     </div>
     <button onClick={this.handleSubmit.bind(this)} type="submit" className="btn btn-primary">Submit</button>
                </div>
