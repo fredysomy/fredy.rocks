33176a0efe21e6dd70452f363963499a98eda4b4
Fredy Somy <fredysomy@gmail.com>
2021-04-18 15:18:51 +0530

    Update three.js

--- src/threejs/three.js
+++ src/threejs/three.js
@@ -53,8 +53,8 @@ function scenery() {
     renderer.render( scene, camera );
   };
 window.addEventListener("scroll",(e)=>{
-  grp.rotation.x+=0.009
-  grp.rotation.y+=0.009
+  grp.rotation.x+=0.09
+  grp.rotation.y+=0.09
   
   
 })
