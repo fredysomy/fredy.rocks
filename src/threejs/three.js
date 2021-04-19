936bf9ff88e3e8255fa35f2901be6d47a76626e9
fredysomy <fredysomy@gmail.com>
2021-04-19 07:41:17 +0000

    added new objects

--- src/threejs/three.js
+++ src/threejs/three.js
@@ -8,28 +8,63 @@ function scenery() {
     0.1,
     1000
   )
-  camera.position.set(0,0,10)
+  camera.position.set(0,0,20)
   const renderer=new THREE.WebGLRenderer({ alpha: true ,canvas:document.querySelector("canvas.webgl")})
   renderer.setSize(window.innerWidth,window.innerHeight)
   
   const geometry = new THREE.BoxGeometry();
   const torus=new THREE.SphereGeometry(0.5,30,30)
+  const directionalLight = new THREE.DirectionalLight( "white", 1 );
+scene.add( directionalLight );
  
-
-  const material = new THREE.MeshNormalMaterial( { color:"cyan" } );
+  const material = new THREE.MeshLambertMaterial({
+    color:"rgb(255,239,0)",
+    emissive: 0xff75,
+    emissiveIntensity: 1,
+    side: THREE.DoubleSide
+});
   material.side = THREE.DoubleSide;
   material.shininess = 100;
+
+   
+  const material1 = new THREE.MeshLambertMaterial({
+    color:"#FF00D5",
+    emissive: 0xff75,
+    emissiveIntensity: 1,
+    side: THREE.DoubleSide
+});
+  material1.side = THREE.DoubleSide;
+  material1.shininess = 100;
  
   const grp=new THREE.Group()
   
 
-  for(var i=0;i<=60;i++){
+  for(var i=0;i<=10;i++){
     //const cube = new THREE.Mesh( geometry, material );
     const torusM=new THREE.Mesh(torus,material)
     torusM.position.set(
-      (Math.random()-0.3)*20,
-      (Math.random()-0.3)*20,
-      (Math.random()-0.3)*20,
+      (Math.random()-0.5)*20,
+      (Math.random()-0.5)*20,
+      (Math.random()-0.5)*20,
+
+    )
+    //cube.position.set(
+     // (Math.random()-0.5)*20,
+     // (Math.random()-0.5)*20,
+    //  (Math.random()-0.5)*20,
+
+    
+    grp.add(torusM)
+
+
+  }
+  for(var j=0;j<=10;j++){
+    //const cube = new THREE.Mesh( geometry, material );
+    const torusM=new THREE.Mesh(torus,material1)
+    torusM.position.set(
+      (Math.random()-0.5)*20,
+      (Math.random()-0.5)*20,
+      (Math.random()-0.5)*20,
 
     )
     //cube.position.set(
